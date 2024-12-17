d6b8b4969826c8f47c735921c14ae88a8e4c9cbf
Sviatoslav Murzin <mursvyats@gmail.com>
2024-12-17 14:53:16 +0500

    chore: deleted workflow

--- hello/main.rs
+++ hello/main.rs
@@ -1,7 +1,22 @@
+// Just trying out comments in Rust
+
 fn main() {
+    format_print();
     hello();
 }
 
 fn hello() {
     println!("Hello, world!");
+}
+
+fn format_print() {
+    println!("{} days", 31);
+
+    // named arguments
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
 }
